dc732bc2ff8e45d683d644e1a6b5901a7c13447e
giosciafra <francescogioscia8@gmail.com>
2024-04-15 18:50:30 +0200

    feat: output errori edit, validazione edit. esercizio completato con i due bonus

--- resources/views/comic/edit.blade.php
+++ resources/views/comic/edit.blade.php
@@ -10,50 +10,104 @@
 
         <div>
           <label for="title" class="form-label">Titolo</label>
-          <input type="text" class="form-control " id="title" name="title" value='{{$comic->title}}'>
+          <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value='{{old('title') ?? $comic->title}}'>
         </div>
+
+        @error('title')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div>
             <label for="description" class="form-label">Descrizione</label>
-            <textarea type="textarea" class="form-control" id="description" name="description"> {{$comic->title}}</textarea>
+            <textarea type="textarea" class="form-control @error('description') is-invalid @enderror" id="description" name="description"> {{old('description') ?? $comic->description}}</textarea>
         </div>
+
+        @error('description')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div>
             <label for="thumb" class="form-label">Aggiungi una URL per inserire la copertina</label>
-            <input type="text" class="form-control" id="thumb" name="thumb" value='{{$comic->thumb}}'>
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value='{{old('thumb') ?? $comic->thumb}}'>
         </div>
+
+        @error('thumb')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div>
             <label for="price" class="form-label">Prezzo ($)</label>
-            <input type="text" class="form-control" id="price" name="price" value='{{$comic->price}}'>
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value='{{old('price') ?? $comic->price}}'>
         </div>
+
+        @error('price')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div>
             <label for="series" class="form-label">Serie</label>
-            <input type="text" class="form-control" id="series" name="series" value='{{$comic->series}}'>
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value='{{old('series') ?? $comic->series}}'>
         </div>
+
+        @error('series')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div>
             <label for="sale_date" class="form-label">Data di pubblicazione</label>
-            <input type="date" class="form-control" id="sale_date" name="sale_date" placeholder='YY-MM-DD' value='{{$comic->sale_date}}'>
+            <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" placeholder='YY-MM-DD' value='{{old('sale_date') ?? $comic->sale_date}}'>
         </div>
+
+        @error('sale_date')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div>
             <label for="type" class="form-label">Tipo di fumetto</label>
-            <input type="text" class="form-control" id="type" name="type" value='{{$comic->type}}'>
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value='{{old('type') ?? $comic->type}}'>
         </div>
+
+        @error('type')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div>
             <label for="artists" class="form-label">Artistə (Disegnatorə)</label>
-            <textarea type="text" class="form-control" id="artists" name="artists" placeholder='Nome e Cognome'>{{$comic->artists}}</textarea>
+            <textarea type="text" class="form-control @error('artists') is-invalid @enderror" id="artists" name="artists" placeholder='Nome e Cognome'>{{old('artists') ?? $comic->artists}}</textarea>
         </div>
+
+        @error('artists')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div>
             <label for="writers" class="form-label">Scrittorə</label>
-            <textarea type="text" class="form-control" id="writers" name="writers" placeholder='Nome e Cognome'>{{$comic->writers}}</textarea>
+            <textarea type="text" class="form-control @error('writers') is-invalid @enderror" id="writers" name="writers" placeholder='Nome e Cognome'>{{old('writers') ?? $comic->writers}}</textarea>
         </div>
 
-        @if($errors->any())
+        @error('writers')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
+
+        {{-- @if($errors->any())
         <div class="alert alert-danger">
             <ul>
                 @foreach($errors->all() as $error)
@@ -61,7 +115,7 @@
                 @endforeach
             </ul>
         </div>
-        @endif
+        @endif --}}
 
         <div class="d-flex justify-content-center gap-4">
 
