f0af73734f42233f2e915f75276f247cc9dd082d
giosciafra <francescogioscia8@gmail.com>
2024-04-17 12:33:35 +0200

    ex-merge

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -108,15 +108,7 @@
         </div>
 
 
-        {{-- @if($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif --}}
+
         <div class="d-flex justify-content-center gap-4 mt-4">
 
             <button type="submit" class="btn btn-primary">Aggiungi</button>
